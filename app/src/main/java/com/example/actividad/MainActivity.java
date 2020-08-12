471ac4ebaecc4e0999d03aa80847f51e06af8c25
gonzalofmora04@gmail.com <gonzalofmora04@gmail.com>
2020-08-11 22:09:48 -0400

    ejercicio numero 2

--- app/src/main/java/com/example/actividad/MainActivity.java
+++ app/src/main/java/com/example/actividad/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.actividad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.prathameshmore.toastylibrary.Toasty;
+
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        if(BuildConfig.DEBUG){
+            Timber.plant(new Timber.DebugTree());
+        }
+
+        Timber.d("on create main actity  "+ this.getClass().getSimpleName());
+        final Toasty toasty = new Toasty(MainActivity.this);
+        Button buton= findViewById(R.id.button_1);
+
+        buton.setOnClickListener(x -> {
+            toasty.successToasty(this,"bien ",Toasty.LENGTH_LONG,Toasty.CENTER);
+        });
+
+     }
 }
