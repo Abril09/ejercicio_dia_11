98d742502a8b6fd99406b14c496911de7aa6f4f4
gonzalofmora@gmail.com <gonzalofmora@gmail.com>
2020-08-12 23:40:43 -0400

    ejercicio 2 y 3

--- app/src/main/java/com/crisspian/fragment_guide_01/MainActivity.java
+++ app/src/main/java/com/crisspian/fragment_guide_01/MainActivity.java
@@ -1,15 +1,33 @@
 package com.crisspian.fragment_guide_01;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.crisspian.fragment_guide_01.databinding.ActivityMainBinding;
 
+import lombok.val;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.button.setOnClickListener((x)->{
+            this.showFragment();
+        });
+    }
+
+    private void showFragment(){
+        val fragment = First_Fragment.newInstance("","");
+        getSupportFragmentManager().beginTransaction()
+                .add(binding.fragmentContainer.getId(),fragment)
+                .addToBackStack(null)
+                .commit();
     }
 
 }
