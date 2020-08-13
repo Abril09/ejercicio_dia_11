72dc72b76b8a102d1b03d8ebc93f1bbc976b5c16
gonzalofmora@gmail.com <gonzalofmora@gmail.com>
2020-08-12 22:25:33 -0400

    primer ejercicio

---
+++ app/src/main/java/com/crisspian/fragment_guide_01/First_Fragment.java
@@ -0,0 +1,48 @@
+package com.crisspian.fragment_guide_01;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class First_Fragment extends Fragment {
+
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public First_Fragment() {
+        // Required empty public constructor
+    }
+
+    public static First_Fragment newInstance(String param1, String param2) {
+        First_Fragment fragment = new First_Fragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first_, container, false);
+    }
+}
